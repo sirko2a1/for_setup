6bb9cfd8424721580db8ea0aed5a3c44cb445bcf
sirko2a1 <134325906+sirko2a1@users.noreply.github.com>
2023-09-11 15:38:51 +0300

    commit1

---
+++ for_setup/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='Personal_assistant',
+    version='0.0.1',
+    description='Group-12 team project',
+    authors='PythonWizards',
+    install_requires=[
+        'dateparser==1.0.0'
+        'prompt_toolkit==3.0.21'
+        'folium==0.12.1'
+        'requests==2.26.0'
+        'pygame==2.0.1'
+        'colorama==0.4.4'
+        ],
+)
+
+# $env:PYTHONPATH = "C:\шлях\до\папки\з_цим_файлом" + $env:PYTHONPATH (треба ввести в першу чергу, вказавши адресу папки з кодом)
+# pip install -r requirements (встановлює всі нетсандартні бібліоткеи що були використані в коді)
+# pip install Personal_assistant (встановлює як пакет)
+# python Personal_assistant.py запускає
+
+
+# pip uninstall my_code (видаляє пакет)
+# pip uninstall -r requirements (видаляє нетсандартні бібліоткеи що були використані в коді)
